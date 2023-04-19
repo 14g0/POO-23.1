dba52fc2b7f63067b0b6d090cbc8092ee90cfb23
Iago <iagocesarts@hotmail.com>
2023-04-19 16:26:57 -0300

    Classe supostamente pronta | Resta horas trabalhadas iniciais

--- Lista_2/Funcionario.java
+++ Lista_2/Funcionario.java
@@ -1,45 +1,82 @@
 public class Funcionario {
     private String nome = "";
 
-    private double salarioHora = 0, horasTrabalhadasMes = 0;
-    double salarioHoraMinimo = 0;
+    double salarioHoraMinimo = 12.5;
+    private double salarioHora = salarioHoraMinimo, horasTrabalhadasMes = 0;
+    
+    private static int proxID = 1;
+    private final int ID_FUNC = proxID;
 
-    private int ID_FUNC = -1, proxID = -1;
-
-    /*----------------------------------------------------------*/
+    /*------------------------ Construtores -------------------------*/
 
     Funcionario(String nome) {
         this.nome = nome;
+        proxID += 1;
     }
 
     Funcionario(String nome, double salarioHM) {
         this.nome = nome;
         this.salarioHoraMinimo = salarioHM;
+        proxID += 1;
     }
 
-    /*----------------------------------------------------------*/
+    /*----------------------------- Get -----------------------------*/
 
-    public String getNome() {
-        return this.nome;
-    }
+    public String getNome() { return this.nome; }
 
-    public int getId() {
-        return ID_FUNC;
-    }
+    public int getId() { return this.ID_FUNC; }
 
-    public double getSalarioHora() {
-        return salarioHora;
-    }
+    public double getSalarioHora() { return this.salarioHora; }
 
-    /*----------------------------------------------------------*/
+    public int getProxID() { return proxID; }
 
-    public double calculaSalarioMes() {
-        double salarioHora = this.salarioHora;
-
-        return salarioHora;
-    }
-
+    /*----------------------------- Set -----------------------------*/
+    
     public void atualizaNome(String nome) {
-        this.nome = nome;
+        String[] nomes = nome.split(" ");
+
+        if(nomes.length <= 1) {
+            System.out.printf("\033[32mNome inválido\033[m");
+        }
+        else {
+            for(String i : nomes) {
+                if(i.length() < 2) {
+                    System.out.println("Nome inválido");
+                    return;
+                }
+            }
+            this.nome = nome;
+        }
+    }
+    
+    public void atualizaSalarioHora(double salarioHora) {
+        if(salarioHora <= this.salarioHora) {
+            System.out.println("Salário menor ou igual ao antigo.");
+        }
+        else this.salarioHora = salarioHora;
+    }
+    
+    /*-------------------- Métodos auxiliativos ---------------------*/
+
+    public void reiniciaMesTrabalho() {
+        this.horasTrabalhadasMes = 0;
+    }
+    
+    public double calculaSalarioMes() {
+        return (this.salarioHora * this.horasTrabalhadasMes);
+    }
+
+    public void adicionaDiaDeTrabalho(double horas) {
+        if(horas >= 4) this.horasTrabalhadasMes += horas;
+        else System.out.println("Hora não computada, minímo de 4h.");
+    }
+
+    public void printFuncionario() {
+        System.out.printf("ID:%05d {\n"+
+            "\tNome: %s\n"+
+            "\tSalario/Hora: %.2f\n"+
+            "\tHoras Trabalhadas: %.2f\n"+
+            "}\n"
+        ,this.ID_FUNC, this.getNome(), this.getSalarioHora(), this.horasTrabalhadasMes);
     }
 }
--- Lista_2/Main.java
+++ Lista_2/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\033\143");
 
-        Funcionario Gabriel = new Funcionario("Gabriel", 25.00);
-        Funcionario Iago = new Funcionario("Iago");
-
+        Funcionario iago = new Funcionario("Iago", 25);
+        Funcionario gabriel = new Funcionario("gabriel", 10);
+        
+        iago.printFuncionario();
+        gabriel.printFuncionario();
     }
 }
